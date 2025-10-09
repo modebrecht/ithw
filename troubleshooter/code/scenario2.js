c59426e969e06771a720d548e67cd2ae219ea94d
modebrecht <matthias.odebrecht@gmail.com>
2025-10-09 12:02:19 +0200

    fix Szenario2

--- troubleshooter/code/scenario2.js
+++ troubleshooter/code/scenario2.js
@@ -2,7 +2,7 @@ export default {
   id: 'E1',
   name: 'Kein Signal',
   storyTitle: 'Szenario: PC läuft, kein Bild',
-  storyText: 'Der PC läuft (Lüfter/LED), aber wir haben kein Bild. <b>Was überprüfst du als Erstes?</b>',
+  storyText: 'Der PC ist angeschaltet. Lüfter hörbar, LED leuchtet grün, aber wir haben kein Bild. <b>Was überprüfst du als Erstes?</b>',
   hints: {
     h1: 'Was beeinflusst das <b>Bild</b>, ohne den PC zu öffnen?',
     h2: '<ul class="hint"><li><b>Anzeige:</b> Monitor einschalten.</li><li><b>Quelle/Kabel:</b> HDMI/DP richtig wählen oder Kabel fest einstecken.</li></ul>'
