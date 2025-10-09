0eb3f539b5e6edf6fcf52e40389ca4c1b85da592
modebrecht <matthias.odebrecht@gmail.com>
2025-10-09 12:11:43 +0200

    fix Scenario3

--- troubleshooter/code/scenario3.js
+++ troubleshooter/code/scenario3.js
@@ -2,7 +2,7 @@ export default {
   id: 'M3',
   name: 'Kein Bild trotz Start',
   storyTitle: 'Szenario: Kein Bild trotz Start',
-  storyText: 'Der PC läuft (Lüfter/LED), der Monitor ist an – doch es erscheint <i>kein Bild</i> (No Signal). <b>Was prüfst du als Nächstes?</b>',
+  storyText: 'Du hast kürzlich neue Komponenten in deinen PC integriert. Du schaltest den PC an und er läuft. Die Lüfter sind hörbar, LED leuchtet grün und der Monitor ist angeschaltet. Jedoch haben haben wir <i>kein Bild</i> (No Signal). Was ist ein häufig auftretender Fehler bei einem Komponenten-Upgrade? <b>Was prüfst du als Nächstes?</b>',
   hints: {
     h1: 'Vor dem Bild passiert der <b>POST</b>. Was verraten <b>Pieptöne/LEDs</b>?',
     h2: '<ul class="hint"><li><b>POST-Hinweise lesen:</b> DRAM-LED an? Beepcode für Speicher?</li><li><b>RAM prüfen:</b> Riegel korrekt eindrücken/einzeln testen.</li><li><b>Anzeige nur sekundär:</b> Monitor kann an sein, aber ohne POST gibt es kein Signal.</li></ul>'
